acb312fbf868c1afa0e580ba73035cb5357e099b
Shang-chi038 <cezeifedigbo-obiora.2303525@stu.cu.edu.ng>
2026-05-28 22:05:07 +0100

    Using .gitignore I removed my test files.
    I have also added files for my landing page,
    index.html (for the web's structure),
    stylesheet.css (for the styling)
    and ns_logo.png (for my logo).

--- test_env.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print("EMAIL_SENDER:", os.getenv('EMAIL_SENDER'))
-print("EMAIL_PASSWORD:", os.getenv('EMAIL_PASSWORD')[:4] + "****")  # Only show first 4 chars
-print("EMAIL_RECIPIENTS:", os.getenv('EMAIL_RECIPIENTS'))
